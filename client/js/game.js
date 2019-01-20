60349162c13765d52b6f48feae8cc928f1fd3b7a
Glarity <tolson519@gmail.com>
2019-01-20 02:59:40 -0800

    Wait for window load to start drawing

--- client/js/game.js
+++ client/js/game.js
@@ -1,40 +1,42 @@
 var otherPlayers = [];
 var map = [
-	[]
+  []
 ];
 
-requestAnimationFrame(loop);
+window.addEventListener('load', function () {
+  requestAnimationFrame(loop);
+});
 
 function loop() {
-	draw();
-	requestAnimationFrame(loop);
+  draw();
+  requestAnimationFrame(loop);
 }
 
 function setDir(newDir) {
-	if (localPlayer.dir != newDir) {
-		localPlayer.dir = newDir;
-		updateDir(localPlayer.dir);
-	}
+  if (localPlayer.dir != newDir) {
+    localPlayer.dir = newDir;
+    updateDir(localPlayer.dir);
+  }
 }
 
-window.addEventListener("keydown", function(event) {
-	if (!playing)
-		return;
-	switch (event.keyCode) {
-		case 65: //a
-			setDir("left");
-			break;
-		case 87: //w
-			setDir("up");
-			break;
-		case 68: //d
-			setDir("right");
-			break;
-		case 83: //s
-			setDir("down");
-			break;
-		case 80: //p
-			setDir("stop");
-			break;
-	}
+window.addEventListener("keydown", function (event) {
+  if (!playing)
+    return;
+  switch (event.keyCode) {
+  case 65: //a
+    setDir("left");
+    break;
+  case 87: //w
+    setDir("up");
+    break;
+  case 68: //d
+    setDir("right");
+    break;
+  case 83: //s
+    setDir("down");
+    break;
+  case 80: //p
+    setDir("stop");
+    break;
+  }
 });
